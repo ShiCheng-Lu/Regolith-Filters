3a19430d8366be09bfa7329edf24d0bcd23ee889
ShiCheng Lu <shichenglu0708@gmail.com>
2021-12-06 21:06:37 -0500

    changed settings and added logs and warnings

--- module_importer/index.js
+++ module_importer/index.js
@@ -4,29 +4,53 @@ const fse = require("fs-extra");
 
 const defSettings = {
     package_path: ".",
-    native_modules: [
-        "mojang-minecraft",
-        "mojang-gametest",
-    ]
+    exclude_modules: [
+        "@types",
+        ""
+    ],
+    silent: true,
+}
+const settings = Object.assign(
+    defSettings,
+    process.argv[2] ? JSON.parse(process.argv[2]) : {}
+);
+
+const included_modules = []
+
+const package = JSON.parse(fs.readFileSync(`../../${settings.package_path}/package.json`).toString());
+// add dependencies to output dir
+for (const module in package.dependencies) {
+    // cp node_modules/[module_name]/[exports] -> [target]/[exports]
+    const module_path = `../../${settings.package_path}/node_modules/${module}`
+
+    try {
+        const config = JSON.parse(fs.readFileSync(`${module_path}/minecraft-module.config.json`).toString());
+        for (const key in config.exports) {
+            fse.copySync(`${module_path}/${key}`, config.exports[key]);
+        }
+        included_modules.push(module);
+        console.log(`resolved module "${module}""`);
+    } catch (err) {
+        // minecraft-module.config.json not found
+        if (!settings.silent) {
+            console.warn(`unable to resolve module "${module}", ${module}/minecraft-module.config.json not found`);
+        }
+    }
 }
 
-const settings = Object.assign(defSettings, JSON.parse(process.argv[2]));
-
-const native_modules_string = settings.native_modules.join("|")
 
 /**
  * replace all module imports with correct relative imports
- * @param {*} err 
- * @param {*} files_names 
+ * @param Error err 
+ * @param string[] files_names 
  */
 function reImportFile(err, files_names) {
     const depth_offset = 3;
     for (const file_name of files_names) {
-        console.log(file_name);
         const file = fs.readFileSync(file_name);
         const depth = file_name.split("/").length - depth_offset;
         const changed_file = file.toString().replace(
-            new RegExp(`(import|export) (.*) from \"((?!${native_modules_string}|\.\/)[^\/\n]*).*\"`, "g"),
+            new RegExp(`(import|export) (.*) from \"(${included_modules.join("|")})\"`, "g"),
             `$1 $2 from "${depth === 0 ? "./" : "../".repeat(depth)}modules/$3/index.js"`
         )
         fs.writeFileSync(file_name, changed_file);
@@ -35,17 +59,3 @@ function reImportFile(err, files_names) {
 
 glob("BP/scripts/**/*.js", { ignore: ["BP/scripts/modules/**/*", "BP/scripts/server/**/*", "BP/scripts/client/**/*"] }, reImportFile);
 
-console.log(`package path: |${settings.package_path}|`);
-
-const package = JSON.parse(fs.readFileSync(`../../${settings.package_path}/package.json`).toString());
-
-// add dependencies to output dir
-for (const module in package.dependencies) {
-    // cp node_modules/[module_name]/[exports] -> [target]/[exports]
-    const module_path = `../../${settings.package_path}/node_modules/${module}`
-
-    const config = JSON.parse(fs.readFileSync(`${module_path}/minecraft-module.config.json`).toString());
-    for (const key in config.exports) {
-        fse.copySync(`${module_path}/${key}`, config.exports[key]);
-    }
-}
--- module_importer/readme.md
+++ module_importer/readme.md
@@ -5,5 +5,5 @@ A regolith filter for import
 | Setting           | Type                                                                       | Default                                 | Description                                                 |
 | ----------------- | -------------------------------------------------------------------------- | --------------------------------------- | ----------------------------------------------------------- |
 | `package_path`    | file path                                                                  | "." (project root)                      | Specify the location of the node package and modules folder |
-| `native_modules`  | array                                                                      | ["mojang-minecraft", "mojang-gametest"] | import modules that should be ignored by the filter         |
-| `compilerOptions` | [compilerOptions](https://www.typescriptlang.org/tsconfig#compilerOptions) | {}                                      | Specifies compiler options for the generated tsconfig.json  |
+| `exclude_modules`  | string[]                                                                  | ["@types", "minecraft-scripting-types-server", "minecraft-scripting-types-client", "minecraft-scripting-types-shared"] | modules that should be ignored by the filter         |
+| `silent` | boolean | true                                      | specify whether module import errors should be logged |
